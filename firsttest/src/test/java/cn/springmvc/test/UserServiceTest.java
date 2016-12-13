2c384f5d5a89600c6503b16523822548ce43a573
x872731861 <x872731861@163.com>
2016-12-13 16:34:03 +0800

    2016.12.13

--- firsttest/src/test/java/cn/springmvc/test/UserServiceTest.java
+++ firsttest/src/test/java/cn/springmvc/test/UserServiceTest.java
@@ -1,15 +1,17 @@
 package cn.springmvc.test;
 
+import java.io.UnsupportedEncodingException;
 import java.util.Date;
+
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 
-import cn.springmvc.dao.PerUserMapper;
 import cn.springmvc.model.PerUser;
 import cn.springmvc.service.PerUserService;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
@@ -24,22 +26,21 @@ public class UserServiceTest extends AbstractTransactionalJUnit4SpringContextTes
 	@Autowired
 	private PerUserService perUserService;
 	
-	@Autowired
-	private PerUserMapper perUserMapper;
-	
 	@Test
+	@Rollback(false)
 	public void PerUserTest () {
 		logger.debug("*********测试开始了");
 		Date date=new Date();
 		PerUser user=new PerUser();
 		user.setUserName("x872731861");
 		user.setPassWord("123456");
-		user.setAge("17");
-		user.setAddress("江西九江");
+		user.setAge("18");
+		String str="江西九江";
+		user.setAddress(str);
 		user.setCreateTime(date);
 		user.setUpdateTime(date);
 		try{
-			int count=perUserMapper.insertSelective(user);
+			int count=perUserService.insertSelective(user);
 			System.out.println("更新了 ："+count+" 条数据");
 		}catch(Exception e){
 			e.printStackTrace();
