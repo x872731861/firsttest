81450db8d2e4d15dc9c58466764b7af92511b629
x872731861 <x872731861@163.com>
2016-12-13 16:40:00 +0800

    2016.12.14解决了数据库乱码的问题

--- firsttest/src/main/java/cn/springmvc/controller/PerUserController.java
+++ firsttest/src/main/java/cn/springmvc/controller/PerUserController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import cn.springmvc.dao.PerUserMapper;
 import cn.springmvc.model.PerUser;
 import cn.springmvc.service.PerUserService;
 
